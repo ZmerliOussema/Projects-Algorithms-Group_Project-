5e89281707c82b66817b86ca05e3cf1d173eb3a4
omarfrikha96 <omar.frikha96@gmail.com>
2024-01-18 14:50:41 +0100

    add application architecture

---
+++ Employees_Leaves/src/main/java/com/Employees_Leaves/EmployeesLeavesApplication.java
@@ -0,0 +1,13 @@
+package com.Employees_Leaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesLeavesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesLeavesApplication.class, args);
+	}
+
+}
---
+++ Employees_Leaves/src/test/java/com/Employees_Leaves/EmployeesLeavesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Employees_Leaves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesLeavesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
